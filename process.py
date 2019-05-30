1dacb76b494cc91d950943242acbb6568c61312e
Jacob <Jacob@wifi-131-179-26-85.host.ucla.edu>
2019-05-29 17:17:34 -0700

    Second Commit

--- process.py
+++ process.py
@@ -4,10 +4,15 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import tensorflow as tf
+
 
 def import_mat(folder_path, file_list):
 	for file in file_list:
-		mat = scipy.io.loadmat(folder_path + "/" + file)
+		file_path = folder_path + "/" + file
+		print(file)
+		mat = scipy.io.loadmat(file_path)
+		
 		# print(mat['X'].shape)
 		# for k in mat.keys():
 		# 	if not k.startswith('__'):
@@ -15,15 +20,22 @@ def import_mat(folder_path, file_list):
 		inputs = extract_pixels(mat['X'], 0, 0)
 		print(inputs)
 		label = mat['CBF'][0][0]
-		print(label)
+		#print(label)
 		for i in range(20):
 			# The greyscale of picture are not on the same level
-			# Do we need to standardize them to the same level?
+			# Do we need to standardize them to the same level?  normalization.
 			img = mat['X'][:, :, i]
+			img = tf.keras.utils.normalize(img,axis =1)
+			print(img)
 			plt.figure()
-			plt.imshow(img, cmap='gray')
+			plt.imshow(img,cmap = plt.cm.binary)
 			plt.show()
 		# print(len(inputs))
+
+def listdir_nohidden(path):
+	for f in os.listdir(path):
+		if not f.startswith('.'):
+			yield f
 
 def extract_pixels(images, h_cord, w_cord):
 	_, _, n = images.shape
@@ -39,7 +51,9 @@ if __name__ == "__main__":
 		print("Usage: python3 process.py [path of data folder]")
 		exit()
 	folder_path = sys.argv[1]
-	data_list = os.listdir(folder_path)
+	folder_path = os.path.abspath(folder_path)
+	data_list = listdir_nohidden(folder_path)
+	print(data_list)
 	import_mat(folder_path, data_list)
 
 
