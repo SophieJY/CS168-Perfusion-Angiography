8f1872ff96f56fd2885decef60d7f1ecfe57cd42
Jiayu Zhao <jyzhao1230@g.ucla.edu>
2019-06-03 02:04:52 -0700

    Added Model

--- process.py
+++ process.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import tensorflow as tf
+from sklearn.kernel_ridge import KernelRidge
+from sklearn import model_selection
+import pickle
 
 
 def import_mat(folder_path, file_list):
@@ -36,11 +39,34 @@ def import_mat(folder_path, file_list):
 			# plt.show()
 		# print(len(inputs))
 
+def import_data(inp, outp):
+	input_array = np.asarray(inp, dtype=np.float32)
+	output_array = np.asarray(outp, dtype=np.float32)
+	# print("Input array shape: ")
+	# print(input_array.shape)
+	# print("Output array shape: ")
+	# print(output_array.shape)
+	krr_model = build_model(input_array, output_array)
+	performance_measure(krr_model, input_array, output_array)
+
+def build_model(input_train, label_train):
+	#!!!May need to change the kernel function and alpha value
+	kr = KernelRidge(kernel="poly", degree=6, alpha=1).fit(input_train, label_train)
+	return kr
+
+def predict_result(model, input_data):
+	return model.predict(input_data)
+
+def performance_measure(model, input_data, output_data):
+	n = len(input_data)
+	MSEs_KRR = model_selection.cross_val_score(model, input_data, output_data, cv=model_selection.LeavePOut(p=1), scoring="neg_mean_squared_error")
+	MeanMSE_KRR = np.mean(list(MSEs_KRR))
+	print("MSE: " + MeanMSE_KRR)
+
 def listdir_nohidden(path):
 	for f in os.listdir(path):
 		if not f.startswith('.'):
 			yield f
-
 
 def extract_pixels(images, h_cord, w_cord):
 	_, _, n = images.shape
@@ -50,16 +76,18 @@ def extract_pixels(images, h_cord, w_cord):
 	# input_array.append(h_cord)
 	# input_array.append(w_cord)
 	return input_array
+
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print("Usage: python3 process.py [path of data folder]")
+	if len(sys.argv) < 3:
+		print("Usage: python3 process.py [input_file_path] [output_file_path]")
 		exit()
-	folder_path = sys.argv[1]
-	folder_path = os.path.abspath(folder_path)
-	# print(os.listdir(folder_path))
-	data_list = listdir_nohidden(folder_path)
-	#print(data_list)
-	import_mat(folder_path, data_list)
+	input_file_path = sys.argv[1]
+	output_file_path = sys.argv[2]
+	with open(input_file_path,'rb') as f:	
+		inp = pickle.load(f)
+	with open(output_file_path,'rb') as v:
+		outp = pickle.load(v)
+	import_data(inp, outp)
 
 
 
